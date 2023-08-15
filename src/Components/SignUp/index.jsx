4db18e660e73e920cbc6e27044fc696c72d51e1b
SWS-5007 <119567356+SWS-5007@users.noreply.github.com>
2023-08-15 01:54:10 -0400

    Added the basic countdown

--- src/Components/SignUp/index.jsx
+++ src/Components/SignUp/index.jsx
@@ -1,7 +1,29 @@
 import React, { useState, useEffect, useCallback } from "react";
+import Countdown from "react-countdown";
 
 import "./styles.css";
 
+const Completionist = () => <span>You are good to go!</span>;
+
+const renderer = ({ days, hours, minutes, seconds, completed }) => {
+  if (completed) {
+    return <Completionist />;
+  } else {
+    return (
+      <span>
+        {days} {hours} {minutes} {seconds}
+      </span>
+    );
+  }
+};
+
 export const SignUpComponent = () => {
-  return <div className="signup-wrapper">SingUp</div>;
+  const currentDate = new Date();
+  const targetDate = new Date(currentDate.getFullYear(), 7, 20);
+
+  return (
+    <div className="signup-wrapper">
+      <Countdown date={targetDate} renderer={renderer} />
+    </div>
+  );
 };
