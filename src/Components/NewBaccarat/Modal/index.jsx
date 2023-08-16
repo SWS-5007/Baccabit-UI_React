179a0da675ff5514c3fe3db061ccd518ee9434a5
SWS-5007 <119567356+SWS-5007@users.noreply.github.com>
2023-08-16 02:19:58 -0400

    Finished the basic deposit modal

--- src/Components/NewBaccarat/Modal/index.jsx
+++ src/Components/NewBaccarat/Modal/index.jsx
@@ -68,95 +68,100 @@ const DepositWithdrawTipModal = ({
         </div>
 
         <div className="deposit-modal-content-wrapper">
-          {/* <div className="deposit-modal-depositcontent">
-            <div className="deposit-modal-currency-network-btn-box">
-              <div>
-                Currency
-                <Form.Select size="sm">
-                  <option>USDT</option>
-                </Form.Select>
+          {walletType === "deposit" ? (
+            <div className="deposit-modal-depositcontent">
+              <div className="deposit-modal-currency-network-btn-box">
+                <div>
+                  Currency
+                  <Form.Select size="sm">
+                    <option>USDT</option>
+                  </Form.Select>
+                </div>
+
+                <div>
+                  Network
+                  <Form.Select size="sm">
+                    <option>ETH</option>
+                    <option>BEEP20</option>
+                  </Form.Select>
+                </div>
               </div>
 
-              <div>
-                Network
-                <Form.Select size="sm">
-                  <option>ETH</option>
-                  <option>BEEP20</option>
-                </Form.Select>
+              <div className="deposit-modal-currency-wallet-address-wrapper">
+                <span
+                  style={{
+                    fontSize: "14px",
+                    color: "white",
+                    paddingLeft: "20px",
+                  }}
+                >
+                  Your USDT deposit address
+                </span>
+
+                <div className="deposit-modal-currency-wallet-address-box">
+                  <span>0xdac17f9580d2ee523a220606994597c13d831ec7</span>
+                  <button className="deposit-modal-wallet-address-copy-btn">
+                    <Clipboard2 className="deposit-modal-wallet-address-copy-icon" />
+                  </button>
+                </div>
+              </div>
+
+              <div className="deposit-modal-qrcode-wrapper">
+                <div className="deposit-modal-qrcode-box"></div>
+
+                <div className="deposit-modal-qrcode-text">
+                  Only send USDT on the ETH network to this address
+                  <br /> <br />2 confirmations required
+                </div>
               </div>
             </div>
+          ) : walletType === "withdraw" ? (
+            <div className="deposit-modal-withdrawcontent">
+              <Form.Select size="sm" className="deposit-modal-currency-balance">
+                <option>0.000000000</option>
+              </Form.Select>
 
-            <div className="deposit-modal-currency-wallet-address-wrapper">
-              <span
-                style={{
-                  fontSize: "14px",
-                  color: "white",
-                  paddingLeft: "20px",
-                }}
-              >
-                Your USDT deposit address
-              </span>
+              <div className="deposit-modal-BTC-address-wrapper">
+                <span
+                  style={{
+                    fontSize: "14px",
+                    color: "white",
+                    paddingLeft: "12%",
+                  }}
+                >
+                  BTC Address
+                </span>
 
-              <div className="deposit-modal-currency-wallet-address-box">
-                <span>0xdac17f9580d2ee523a220606994597c13d831ec7</span>
-                <button className="deposit-modal-wallet-address-copy-btn">
-                  <Clipboard2 className="deposit-modal-wallet-address-copy-icon" />
-                </button>
+                <div className="deposit-modal-BTC-address-box">
+                  0xdac17f9580d2ee523a220606994597c13d831ec7
+                </div>
+              </div>
+
+              <div className="deposit-modal-BTC-amount-wrapper">
+                <span
+                  style={{
+                    fontSize: "14px",
+                    color: "white",
+                    paddingLeft: "12%",
+                  }}
+                >
+                  Amount:
+                </span>
+
+                <div className="deposit-modal-BTC-amount-box">
+                  0.00 $
+                  <button className="deposit-modal-BTC-MAK-btn">MAK</button>
+                </div>
+              </div>
+
+              <div className="deposit-modal-withdrawal-text">
+                Your withdrawal will have 0.00000000 subtracted from your
+                remaining balance to cover the transaction fee.
               </div>
             </div>
-
-            <div className="deposit-modal-qrcode-wrapper">
-              <div className="deposit-modal-qrcode-box"></div>
-
-              <div className="deposit-modal-qrcode-text">
-                Only send USDT on the ETH network to this address
-                <br /> <br />2 confirmations required
-              </div>
-            </div>
-          </div> */}
-
-          <div className="deposit-modal-withdrawcontent">
-            <Form.Select size="sm" className="deposit-modal-currency-balance">
-              <option>0.000000000</option>
-            </Form.Select>
-
-            <div className="deposit-modal-BTC-address-wrapper">
-              <span
-                style={{
-                  fontSize: "14px",
-                  color: "white",
-                  paddingLeft: "12%",
-                }}
-              >
-                BTC Address
-              </span>
-
-              <div className="deposit-modal-BTC-address-box">
-                0xdac17f9580d2ee523a220606994597c13d831ec7
-              </div>
-            </div>
-
-            <div className="deposit-modal-BTC-amount-wrapper">
-              <span
-                style={{
-                  fontSize: "14px",
-                  color: "white",
-                  paddingLeft: "12%",
-                }}
-              >
-                Amount:
-              </span>
-
-              <div className="deposit-modal-BTC-amount-box">
-                0.00 $<button className="deposit-modal-BTC-MAK-btn">MAK</button>
-              </div>
-            </div>
-
-            <div className="deposit-modal-withdrawal-text">
-              Your withdrawal will have 0.00000000 subtracted from your
-              remaining balance to cover the transaction fee.
-            </div>
-          </div>
+          ) : (
+            ""
+          )}
         </div>
       </div>
     </Modal>
